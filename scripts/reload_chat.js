28bcf8f54828a000901952882f5041a8fca7775e
KeyTey <k.terada.1211@gmail.com>
2020-12-25 17:32:00 +0900

    Update

--- scripts/reload_chat.js
+++ scripts/reload_chat.js
@@ -1,18 +1,17 @@
 // チャットを更新する
 const reloadChat = () => {
-    const button = $('#show-hide-button paper-button');
-    Promise.resolve()
-    .then(() => new Promise((resolve) => {
-        // チャット非表示ボタンをクリックする
-        if ($(button).length === 0) return;
-        $(button).click();
-        resolve();
-    }))
-    .then(() => new Promise(() => {
-        // チャット表示ボタンをクリックする
-        if ($(button).length === 0) return;
-        $(button).click();
-    }));
+    const frame = $('ytd-live-chat-frame');
+    const button = $(frame).find('#show-hide-button paper-button');
+    if ($(frame).length === 0) return;
+    if ($(button).length === 0) return;
+    // チャットの初期状態を取得する
+    const collapsed = $(frame).attr('collapsed') !== undefined;
+    // チャット切替ボタンをクリックする
+    $(button).click();
+    // 初期状態が非表示の場合 -> キャンセル
+    if (collapsed) return;
+    // チャット切替ボタンをクリックする
+    $(button).click();
 };
 
 // メッセージイベント -> チャットを更新する
