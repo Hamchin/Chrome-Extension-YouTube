58d38a8621d16a978b30e721d2c29b3328d5abb5
KeyTey <k.terada.1211@gmail.com>
2021-01-09 02:30:30 +0900

    Update

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 - Extend the space of the video title.
 - Prevent the channel video from being played.
-- Reset the volume each time a new tab is opened.
 - Place the boxed comments next to the video.
 - Pin the video in a floating frame when scrolling down.
-- Click the button to filter the live chat.
+- Press <kbd>[</kbd> / <kbd>]</kbd> to backward / forward 1 second.
+- Reset the volume each time a new tab is opened.
 - Click the button to reload the live chat.
-- Click the button to show the video / broadcast information for subscriptions.
+- Click the button to filter the live chat.
+- Click the button to filter the list of videos for subscriptions.
--- scripts/video_control.js
+++ scripts/video_control.js
@@ -1,18 +1,25 @@
-// キーダウンイベント: ドキュメント
-$(document).on('keydown', (e) => {
-    if (location.pathname !== '/watch') return;
+// キーダウンイベント: 動画ページ
+const handleVideoKeyDown = (e) => {
     if (e.altKey || e.ctrlKey || e.shiftKey || e.metaKey) return;
-    // 音量を調節する
+    // 上下キーの場合 -> 音量を調節する
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
         e.preventDefault();
-        // 既にプレーヤーにフォーカス中の場合 -> キャンセル
+        // プレーヤーにフォーカス中の場合 -> キャンセル
         const player = document.querySelector('.html5-video-player');
+        if (player === null) return;
         if (document.activeElement.isEqualNode(player)) return;
         // プレーヤーにキーイベントを送信する
         const keyEvent = new KeyboardEvent('keydown', { keyCode: e.keyCode });
         player.dispatchEvent(keyEvent);
     }
-});
+    // 角括弧キーの場合 -> 動画を1秒シークする
+    if (e.key === '[' || e.key === ']') {
+        const video = document.querySelector('ytd-player video');
+        if (video === null) return;
+        const time = (e.key === '[') ? -1 : (e.key === ']') ? 1 : 0;
+        video.currentTime += time;
+    }
+};
 
 // キーダウンイベント: テキストエリア -> 親要素への伝播を抑止する
 $(document).on('keydown', 'input, textarea, ytd-commentbox', (e) => e.stopPropagation());
@@ -20,11 +27,17 @@ $(document).on('keydown', 'input, textarea, ytd-commentbox', (e) => e.stopPropag
 // 変更監視: 音量パネル -> 音量のキャッシュを削除する
 const volumePanelObserver = new MutationObserver(() => localStorage.removeItem('yt-player-volume'));
 
-// タブ更新イベント -> 変更監視を開始する
+// タブ更新イベント
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type !== 'UPDATED') return;
+    // 設定をリセットする
+    window.removeEventListener('keydown', handleVideoKeyDown);
     volumePanelObserver.disconnect();
+    // 動画ページ以外の場合 -> キャンセル
     if (location.pathname !== '/watch') return;
+    // キーダウンイベントを登録する
+    window.addEventListener('keydown', handleVideoKeyDown);
+    // 変更監視を開始する
     const panel = document.querySelector('.ytp-volume-panel');
     if (panel === null) return;
     const options = { attributes: true };
